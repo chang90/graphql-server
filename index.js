b9c6e9b8f305433424cdf8f82c9900c2e183a53c
hiby90hou <chang.liu.programmer@gmail.com>
2020-10-18 18:07:26 +1100

    able to use nesting resolvers

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { ApolloServer, gql } = require('apollo-server');
 const BookAPI = require('./datasources/books');
+const SpeakerAPI = require('./datasources/speakers');
 
 // A schema is a collection of type definitions (hence "typeDefs")
 // that together define the "shape" of queries that are executed against
@@ -8,7 +9,8 @@ const typeDefs = require('./schema.js')
 const resolvers = require('./resolvers.js')
 
 const dataSources = () =>({
-  bookAPI: new BookAPI()
+  bookAPI: new BookAPI(),
+  speakerAPI: new SpeakerAPI()
 });
 
 const server = new ApolloServer({ typeDefs, resolvers, dataSources });
--- resolvers.js
+++ resolvers.js
@@ -1,10 +1,27 @@
+const _ = require('lodash');
+
 module.exports = {
   Query: {
     books: (parent, args, {dataSources}, info) => {
       return dataSources.bookAPI.getBooks(args);
     },
-    bookById: (parent, {id}, {dataSources}, info) =>{
+    bookById: (parent, {id}, {dataSources}, info) => {
       return dataSources.bookAPI.getBookById(id);
-    }
+    },
+    speakers: (parent, args, {dataSources}, info) => {
+      return dataSources.speakerAPI.getSpeakers();
+    },
+    speakerById: (parent, {id}, {dataSources}, info) => {
+      return dataSources.speakerAPI.getSpeakerById(id);
+    },
   },
+  Book: {
+    async speakers(session, args, {dataSources}, info) {
+      const speakers = await dataSources.speakerAPI.getSpeakers();
+      const returns = speakers.filter((speaker) =>{
+        return _.filter(session.speakers, {id: speaker.id}).length > 0;
+      })
+      return returns;
+    }
+  }
 };
